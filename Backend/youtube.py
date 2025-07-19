5ca0030d94b0e333e6fd8893c543bf9e2f21791a
Hashir Ali <hashirali12123@gmail.com>
2025-07-19 17:02:42 +0500

    second change

--- Backend/youtube.py
+++ Backend/youtube.py
@@ -1,1 +1,16 @@
 from yt_dlp import YoutubeDL
+
+class YoutubeDownloader:
+    def __init__(self):
+        pass
+
+    def download_video(self,url: str, output_path: str = "downloads/%(title)s.%(ext)s"):
+        path = {
+            'outtmpl': output_path,  
+        }
+        with YoutubeDL(path) as loader:
+            loader.download([url])
+
+testing= YoutubeDownloader()
+testing.download_video("https://youtu.be/668nUCeBHyY?si=hVY9SBc2Ps0rvDY0", "downloads/%(title)s.%(ext)s")
+        
