7ce0b552c0535b91a62abb30690236b7be156ee5
Hashir Ali <hashirali12123@gmail.com>
2025-07-30 14:50:30 +0500

    removing the clip function for now

--- Backend/youtube.py
+++ Backend/youtube.py
@@ -4,9 +4,9 @@ class YoutubeDownloader:
     def __init__(self):
         pass
 
-    def download_by_resolution(self, url: str, resolution: str = '720',filename:str="", output_path: str = "downloads"):
+    def download_by_resolution(self,url: str, resolution: str = '720', filename: str = "", output_path: str = "downloads"):
         options = {
-            'format': f'bestvideo[height<={resolution}]+bestaudio/best',
+            'format': f'bestvideo[height<={resolution}]+bestaudio[ext=m4a]/best',
             'outtmpl': f"{output_path}/{filename}.%(ext)s" if filename else f"{output_path}/%(title)s.%(ext)s",
             'merge_output_format': 'mp4',
         }
@@ -29,7 +29,19 @@ class YoutubeDownloader:
 
     def get_video_info(self, url: str) -> dict:
         with YoutubeDL({'quiet': True}) as loader:
-            return loader.extract_info(url, download=False)
+            info = loader.extract_info(url, download=False)
+
+        return {
+            "id": info.get("id"),
+            "title": info.get("title"),
+            "duration_seconds": info.get("duration"),
+            "webpage_url": info.get("webpage_url"),
+            "resolution": (
+                f"{info.get('width')}x{info.get('height')}"
+                if info.get("width") and info.get("height")
+                else "Unknown"
+            ),
+        }
     
 
 
@@ -44,25 +56,9 @@ class YoutubeDownloader:
         with YoutubeDL(options) as loader:
             loader.download([playlist_url])
 
-    def download_clip(self, url: str, start_time: str, duration: str,filename:str="", output_path: str = "downloads"):
-        """
-        Downloads a video clip starting at `start_time` for `duration` seconds.
-        """
-        options = {
-            'outtmpl': f"{output_path}/{filename}.%(ext)s" if filename else f"{output_path}/%(title)s.%(ext)s",
-            'merge_output_format': 'mp4',
-            'postprocessor_args': ['-ss', start_time, '-t', duration],
-        }
-
-        with YoutubeDL(options) as loader:
-            loader.download([url])
-
 
 path=r"D:\ML\Flagship\testing"
 
-if __name__ == "__main__":
-    downloader = YoutubeDownloader()
-    downloader.download_by_resolution("https://youtu.be/QC8iQqtG0hg?si=QYfNVaylojYYg_Z-", resolution='360', filename="test_video", output_path=path)
 
 
 
